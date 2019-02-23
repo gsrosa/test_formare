7bbe01e2659563f65818519e8c0e14fadff25e1d
guilherme-pollenapp <guilherme@pollenapp.com.br>
2019-02-23 11:21:24 -0300

    list users

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import axios from "axios";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = ({}) => {
+  const [users, setUsers] = React.useState([]);
+
+  React.useEffect(() => {
+    axios.get("http://localhost:3000/generate").then(response => {
+      setUsers(response.data);
+    });
+  }, []);
+
+  return (
+    <div className="app">
+      {users.map(user => (
+        <div className="user-card">
+          <img src={user.picture} />
+          <p>{user.name}</p>
+          <p>{user.email}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default App;
