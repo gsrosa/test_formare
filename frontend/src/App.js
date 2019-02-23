64501f32c7044efacddd2ff1d2e744c65bc54f86
guilherme-pollenapp <guilherme@pollenapp.com.br>
2019-02-23 11:31:34 -0300

    list complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,19 +7,33 @@ const App = ({}) => {
 
   React.useEffect(() => {
     axios.get("http://localhost:3000/generate").then(response => {
+      response.data.sort((a, b) => {
+        return a.name > b.name ? 1 : a.name < b.name ? -1 : 0;
+      });
       setUsers(response.data);
     });
   }, []);
 
   return (
     <div className="app">
-      {users.map(user => (
-        <div className="user-card">
-          <img src={user.picture} />
-          <p>{user.name}</p>
-          <p>{user.email}</p>
-        </div>
-      ))}
+      {users.map(user => {
+        const group = user.name.trim().match(/^[a-f]/g)
+          ? "1"
+          : user.name.trim().match(/^[g-m]/g)
+          ? "2"
+          : user.name.trim().match(/^[n-s]/g)
+          ? "3"
+          : "4";
+
+        return (
+          <div className="user-card">
+            <img src={user.picture} />
+            <p>{user.name}</p>
+            <p>{user.email}</p>
+            <p>{group}</p>
+          </div>
+        );
+      })}
     </div>
   );
 };
